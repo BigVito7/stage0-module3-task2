059260c22bc11b52d5bf1fb9bd1af09ef4b824c4
BigVito7 <biggvito7@gmail.com>
2025-02-25 11:16:41 +0200

    changed the variable names

--- src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
@@ -1,13 +1,13 @@
 package school.mjc.stage0.module3.task2;
 
-public class advancedNamingConvention {
-    private static final int adult_age = 18;
+public class AdvancedNamingConvention {
+    private static final int ADULT_AGE = 18;
 
-    private int AGE;
+    private int age;
     private int phoneNumber;
 
-    void CAllToFriend() {
-        //some code
+    static void callToFriend() {
+        System.out.println("To call Violette after she will be over: " + ADULT_AGE);
     }
 
     void callByNumber(int Number) {
